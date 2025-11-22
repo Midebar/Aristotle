50861292c453da911665d8d8959525129936b97a
Midebar <midebar@gmail.com>
2025-11-23 05:28:18 +0700

    Reapply " Tried ExllamaV2 with custom parquet, doesn't work, instead using GGUF q4_k_m"
    This reverts commit 05a9fecdd846673d3cb8e4fe598988e7c43a1490.

--- llm_backends.py
+++ llm_backends.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8da768de839f9e5fe5fa0c0ae2c78449ba684ae7c83e3ff76d8c37368efc25f6
-size 9057
+oid sha256:994678f100a4c619bab76bf4841aff0b92cf24ee441d0b10cd16765cc39ac1bd
+size 6157
---
+++ misc/speed_test_backend.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:540177f9d3c159ddff6c1aa113e81e99586df6d3b4273dfb1ec710047636d326
+size 4538
